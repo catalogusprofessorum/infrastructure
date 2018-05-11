69379e10d9d60fd241c71a07d68266bf33362eb5
triechert <thomas.riechert@htwk-leipzig.de>
2018-05-11 11:01:56 +0200

    back to ununtu 14.04

--- config/ontowiki/config-run.sh
+++ config/ontowiki/config-run.sh
@@ -7,7 +7,7 @@ ln -s /var/www/extensions/themes/silverblue/scripts /var/www/extensions/themes/h
 
 echo 'themes.default = "heloise"' >> /var/www/config.ini
 # set  extension
-cp /config/extensions/*.ini /var/www/extension/
+cp /config/extensions/*.ini /var/www/extensions/
 
 # copy news rss-feed
 cp /config/IndexController.php /var/www/application/controllers/
